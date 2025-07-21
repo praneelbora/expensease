bf7f4fc1fe9007fb8ddea4827b6e99f79b74f546
Praneel Bora <92789025+praneelbora@users.noreply.github.com>
2025-07-21 18:51:44 +0530

    Update auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,8 +4,6 @@ const User = require('../models/User')
  
 module.exports = async function (req, res, next) {
   const token = req.header('x-auth-token');
-  console.log(token);
-  
   if (!token) return res.status(401).json({ msg: 'No token, authorization denied' });
 
   try {
